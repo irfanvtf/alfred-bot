f45b4cc90041e87022429115963211cbe0d40390
irfan <irfanfmurad@gmail.com>
2025-07-07 09:49:25 +0800

    fix: update wait script

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -3,10 +3,13 @@
 
 set -e
 
+if [ "$#" -lt 3 ]; then
+  echo "Usage: $0 <host> <port> <command> [args...]" >&2
+  exit 1
+fi
 host="$1"
 port="$2"
 shift 2
-cmd="$@"
 
 until nc -z "$host" "$port"; do
   >&2 echo "Redis is unavailable - sleeping"
@@ -14,4 +17,4 @@ until nc -z "$host" "$port"; do
 done
 
 >&2 echo "Redis is up - executing command"
-exec $cmd
+exec "$@"
